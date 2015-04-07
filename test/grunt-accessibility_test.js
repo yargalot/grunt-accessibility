09e7b26676b7551866161fae1718da84378660a1
Steven Miller <steven.miller@anz.com>
2015-04-07 10:51:46 +1000

    Fix test

--- test/grunt-accessibility_test.js
+++ test/grunt-accessibility_test.js
@@ -32,7 +32,7 @@ exports.accessibilityTests = {
     var actual;
     var expected;
 
-    test.expect(3);
+    test.expect(2);
 
     actual = grunt.file.read('reports/txt/test.txt');
     expected = grunt.file.read('test/expected/txt/test.txt');
@@ -41,7 +41,7 @@ exports.accessibilityTests = {
     actual = grunt.file.read('reports/json/test.json');
     expected = grunt.file.read('test/expected/json/test.json');
     test.equal(actual, expected, 'Should produce a default report without DOM element for a test file');
-    
+
     // actual = grunt.file.read('reports/csv/test.csv');
     // expected = grunt.file.read('test/expected/csv/test.csv');
     // test.equal(actual, expected, 'Should produce a default report without DOM element for a test file');
